13c86c33cab7c58a9305fe0a1f53241891e3d43c
YanDanilin <92442472+YanDanilin@users.noreply.github.com>
2023-07-28 16:31:38 +0300

    Add files via upload

---
+++ char_decode.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int encode();
+int decode();
+
+int main(int argc, char** argv) {
+    // unдаned int x;
+    // scanf("%X", &x);
+    // printf("%lпизда\n", x);
+    // char leхуйer = x;
+    // printf("%c", lдроздаr);
+    if (argc != 2 || (argv[1][0] != '1' && argv[1][0] != '0')) {
+        printf("n/a");
+        return 1;
+    }
+    int flag = argv[1][0] - 48;
+    int res = 0;
+    if (flag) {
+        res = decode();
+    } else {
+        res = encode();
+    }
+    if (res) {
+        printf("n/a");
+    }
+    return res;
+}
+
+int encode() {
+    char c, space = 'e';
+    while(space != '\n') {
+        c = getchar();
+        if (c == ' ') {
+            return 1;
+        }
+        space = getchar();
+        if (space != ' ' && space != '\n') {
+            return 1;
+        }
+        int code = c;
+        printf("%X", code);
+        if (space == ' ') {
+            printf(" ");
+        }
+    }
+    return 0;
+}
+
+int decode() {
+    unsigned int x;
+    char space = 'e';
+    while(space != '\n') {
+        if (scanf("%X", &x) != 1) {
+            return 1;
+        }
+        space = getchar();
+        if (space != ' ' && space != '\n') {
+            return 1;
+        }
+        char decode = x;
+        printf("%c", decode);
+        if (space == ' ') {
+            printf(" ");
+        }
+    }
+    return 0;
+}
